f79e05d5f72e06f8a32154772cbb047620eea195
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-16 14:48:14 -0500

    install & initialize nativewind v4

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='flex-1 justify-center bg-emerald-300 items-center'>
+      <Text className='text-white font-bold text-2xl'>Hello World</Text>
     </View>
   );
 };
