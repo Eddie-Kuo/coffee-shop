d912498d96ac01b87a88de5bf867beed52b03ca3
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-17 16:05:11 -0500

    init other tab screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 import "../global.css";
 
+import { useColorScheme } from "nativewind";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
