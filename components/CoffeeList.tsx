785955726f80f3028724bfe059a74770a519f5aa
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-18 16:03:40 -0500

    add ui to categories option

--- components/CoffeeList.tsx
+++ components/CoffeeList.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { FlatList, Text, TouchableOpacity } from "react-native";
+import Colors from "@/constants/Colors";
+import { cn } from "@/lib/clsx";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { FlatList, ListRenderItem, Text, TouchableOpacity } from "react-native";
 
 const CoffeeList = () => {
+  const [category, setCategory] = useState<string>("All");
+
   const categories = [
     "All",
     "Cappuccino",
@@ -9,18 +14,39 @@ const CoffeeList = () => {
     "Americano",
     "Macchiato",
   ];
+
+  const renderCategories: ListRenderItem<string> = (item) => {
+    const selectedCategory = item.item === category;
+    return (
+      <TouchableOpacity
+        onPress={() => setCategory(item.item)}
+        className="items-center gap-2"
+      >
+        <Text
+          className={cn(
+            "text-xl font-semibold text-white",
+            selectedCategory ? "text-orange" : "text-mediumGrey",
+          )}
+        >
+          {item.item}
+        </Text>
+        {selectedCategory && (
+          <Ionicons name="ellipse-sharp" color={Colors.orange} />
+        )}
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <FlatList
-      data={categories}
-      contentContainerClassName="flex-row gap-5"
-      renderItem={(item) => (
-        <TouchableOpacity>
-          <Text className="text-xl font-semibold text-mediumGrey">
-            {item.item}
-          </Text>
-        </TouchableOpacity>
-      )}
-    />
+    <>
+      <FlatList
+        data={categories}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerClassName="gap-5"
+        renderItem={renderCategories}
+      />
+    </>
   );
 };
 
