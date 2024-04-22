f003f197535591cf3b1b478990aee3690ead1baa
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-22 17:27:05 -0500

    set state for product data

--- app/(product)/[productType]/[productId]/index.tsx
+++ app/(product)/[productType]/[productId]/index.tsx
@@ -1,17 +1,38 @@
+import { BeansData, CoffeeData } from "@/constants/Data";
+import { Bean, Coffee } from "@/types";
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 const ProductScreen = () => {
+  const [productData, setProductData] = useState<Coffee | Bean>();
   const { productId, productType } = useLocalSearchParams<{
     productId: string;
     productType: string;
   }>();
 
+  // we need to know where to get our data from between beans and coffee datasets
+  useEffect(() => {
+    if (productType === "Coffee") {
+      const data = CoffeeData.find(
+        (element) => element.id === parseInt(productId),
+      );
+      setProductData(data);
+    } else {
+      const data = BeansData.find(
+        (element) => element.id === parseInt(productId),
+      );
+      setProductData(data);
+    }
+  }, []);
+
+  console.log(productData);
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Product ID: {productId}</Text>
       <Text>Product Type: {productType}</Text>
+      <Text></Text>
     </View>
   );
 };
