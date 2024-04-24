3d52de4d6c4f8c14896e9d439197a593ee45ba37
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-04-24 15:25:50 -0500

    add second tag to product description

--- app/(product)/[productType]/[productId]/index.tsx
+++ app/(product)/[productType]/[productId]/index.tsx
@@ -27,7 +27,9 @@ const ProductScreen = () => {
   }, []);
 
   // * Add a comma into the product total review number
-  const productReviewNumber = productData?.totalReviews.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  const productReviewNumber = productData?.totalReviews
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
     <View className="flex-1 items-center justify-center">
@@ -39,8 +41,7 @@ const ProductScreen = () => {
         />
       </View>
       <View className="w-full basis-3/5 ">
-        <View className="absolute -top-56 h-56 w-full rounded-t-3xl bg-black/50 p-8 gap-5 justify-center">
-
+        <View className="absolute -top-56 h-56 w-full justify-center gap-5 rounded-t-3xl bg-black/50 p-8 ">
           {/*Top row info*/}
           <View className="flex-row items-center justify-between">
             <View>
@@ -51,30 +52,45 @@ const ProductScreen = () => {
                 {productData?.note}
               </Text>
             </View>
-            <View className="items-center justify-center rounded-xl bg-darkGrey p-2">
-              <Image source={require("@/assets/images/coffee.png")} />
-              {productData?.type === "Coffee" ? (
-                <Text className="text-sm text-white">{productData?.type}</Text>
-              ) : (
-                <Text className="text-sm text-white">Bean</Text>
-              )}
+            <View className="flex-row gap-7">
+              <View className="h-16 w-16 items-center justify-center rounded-xl bg-darkGrey">
+                <Image source={require("@/assets/images/coffee.png")} />
+                {productData?.type === "Coffee" ? (
+                  <Text className="text-sm text-white">
+                    {productData?.type}
+                  </Text>
+                ) : (
+                  <Text className="text-sm text-white">Bean</Text>
+                )}
+              </View>
+              <View className="h-16 w-16 items-center justify-center gap-1.5 rounded-xl bg-darkGrey">
+                {productData?.type === "Coffee" ? (
+                  <>
+                    <Image source={require("@/assets/images/liquid.png")} />
+                    <Text className="text-sm text-white">Milk</Text>
+                  </>
+                ) : (
+                  <>
+                    <Image source={require("@/assets/images/location.png")} />
+                    <Text className="text-sm text-white">Africa</Text>
+                  </>
+                )}
+              </View>
             </View>
           </View>
 
           {/*  Bottom row info*/}
           <View className="flex-row justify-between">
-            <View className="flex-row gap-3 items-center">
-              <Image source={require('@/assets/images/star.png')}/>
-              <Text className="text-white font-bold text-3xl">
+            <View className="flex-row items-center gap-3">
+              <Image source={require("@/assets/images/star.png")} />
+              <Text className="text-3xl font-bold text-white">
                 {productData?.rating}
               </Text>
               <Text className={"text-lightGrey"}>({productReviewNumber})</Text>
             </View>
-          <View className="p-4 bg-darkGrey rounded-xl">
-            <Text className='text-white'>
-              {productData?.blend}
-            </Text>
-          </View>
+            <View className="rounded-xl bg-darkGrey p-4">
+              <Text className="text-white">{productData?.blend}</Text>
+            </View>
           </View>
         </View>
         <View>
